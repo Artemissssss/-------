7ed1156da7947f3d124eaf7af8b1592553f42855
Artemissssss <64923345+Artemissssss@users.noreply.github.com>
2021-11-22 20:25:07 +0200

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+Plese include slider.js in your Html
+Make styles and elements like in library Elements
+In img with class files-slider with your src
--- slider.js
+++ slider.js
@@ -1,36 +1,40 @@
 let allElements = []
 let counterSlides = 0;
+let counterSlidesRight = 0;
 let speedAutoSlide = 5000;
 for(let i=0;i<document.getElementsByClassName("files-slider").length;i++){
 allElements.unshift(document.getElementsByClassName("files-slider")[i].src)
 }
 setInterval(() => {
     if (counterSlides < allElements.length) {
-        document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
         counterSlides++;
+        counterSlidesRight++;
+        document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
     } else {
-        document.getElementById("slider-img").src = `${allElements[0]}`;
         counterSlides = 0;
+        counterSlidesRight = 0;
+        document.getElementById("slider-img").src = `${allElements[0]}`;
     }
 }, speedAutoSlide);
 document.getElementsByClassName("pos-slider-class")[0].addEventListener("click", leftSlide);
 
 function leftSlide() {
     if (counterSlides > 0) {
-        counterSlides--;
         document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
     } else{
-        counterSlides = allElements.length;
         document.getElementById("slider-img").src = `${allElements[counterSlides-1]}`;
     }
+    counterSlidesRight = counterSlides;
 }
 document.getElementsByClassName("pos-slider-class-right")[0].addEventListener("click", rightSlide);
 function rightSlide() {
-    if (counterSlides < allElements.length-1) {
-        counterSlides++;
-        document.getElementById("slider-img").src = allElements[counterSlides];
+    if (counterSlidesRight < allElements.length-1 && counterSlidesRight !==allElements.length-1) {
+        counterSlidesRight+=1;
+        counterSlides = counterSlidesRight;
+        document.getElementById("slider-img").src = allElements[counterSlidesRight];
     } else {
-        counterSlides =  0;
-        document.getElementById("slider-img").src = allElements[counterSlides];
+        counterSlidesRight = 0;
+        counterSlides = counterSlidesRight;
+        document.getElementById("slider-img").src = allElements[0];
     }
 }
