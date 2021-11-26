3e77b1af9cdc08649c697d9deafd3ad140eadad6
Artemissssss <64923345+Artemissssss@users.noreply.github.com>
2021-11-26 18:39:36 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,17 +1,17 @@
 let allElements = []
-let counterSlides = 0;
+document.getElementsByClassName("num")[0].textContent = 0;
 let speedAutoSlide = parseInt(document.getElementsByClassName("speed")[0].textContent);
-for(let i=0;i<document.getElementsByClassName("files-slider").length;i++){
-allElements.unshift(document.getElementsByClassName("files-slider")[i].src)
+for (let i = 0; i < document.getElementsByClassName("files-slider").length; i++) {
+    allElements.unshift(document.getElementsByClassName("files-slider")[i].src)
 }
 setInterval(() => {
-    if(allElements.length == 1){
+    let counterSlides = parseInt(document.getElementsByClassName("num")[0].textContent);
+    if (allElements.length == 1) {
         document.getElementById("slider-img").src = allElements[0];
-    } else if (counterSlides < allElements.length-1) {
+    } else if (counterSlides < allElements.length - 1) {
         counterSlides++;
         document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
-    }
-     else {
+    } else {
         counterSlides = 0;
         document.getElementById("slider-img").src = `${allElements[0]}`;
     }
@@ -20,31 +20,32 @@ setInterval(() => {
 document.getElementsByClassName("pos-slider-class")[0].addEventListener("click", leftSlide);
 
 function leftSlide() {
-    counterSlides = document.getElementsByClassName("num")[0].textContent;
+    let counterSlides =  parseInt(document.getElementsByClassName("num")[0].textContent);
     if (counterSlides > 0) {
         counterSlides--;
         document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
-    } else{
-        counterSlides = allElements.length-1;
-        document.getElementById("slider-img").src = `${allElements[counterSlides-1]}`;
+    } else {
+        counterSlides = allElements.length - 1;
+        document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
     }
     document.getElementsByClassName("num")[0].textContent = counterSlides;
 }
 let allElementsRight = []
-let counterSlidesRight = 0;
-for(let i=0;i<document.getElementsByClassName("files-slider").length;i++){
-allElementsRight.unshift(document.getElementsByClassName("files-slider")[i].src)
+
+for (let i = 0; i < document.getElementsByClassName("files-slider").length; i++) {
+    allElementsRight.unshift(document.getElementsByClassName("files-slider")[i].src)
 }
 document.getElementsByClassName("pos-slider-class-right")[0].addEventListener("click", rightSlide);
+let counterSlidesRight =  parseInt(document.getElementsByClassName("num")[0].textContent)
+let helper = []
+helper.unshift(counterSlidesRight)
 function rightSlide() {
-    if (counterSlidesRight < allElementsRight.length-1 && counterSlidesRight !==allElementsRight.length-1) {
-        counterSlidesRight+=1;
-        counterSlides = counterSlidesRight;
-        document.getElementById("slider-img").src = allElementsRight[counterSlidesRight];
+    if (helper[0] < allElementsRight.length - 1 && helper[0] !== allElementsRight.length - 1) {
+        helper[0] += 1;
+        document.getElementById("slider-img").src = allElementsRight[helper[0]];
     } else {
-        counterSlidesRight = 0;
-        counterSlides = counterSlidesRight;
+        helper[0] = 0;
         document.getElementById("slider-img").src = allElementsRight[0];
     }
-    document.getElementsByClassName("num")[0].textContent = counterSlidesRight;
+    document.getElementsByClassName("num")[0].textContent = helper[0];
 }
