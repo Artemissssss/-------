5926a03ca017a3576e2e85dd11589c0baba905c2
Artemissssss <64923345+Artemissssss@users.noreply.github.com>
2021-11-29 17:53:37 +0200

    Add files via upload

--- slider.js
+++ slider.js
@@ -22,32 +22,30 @@ document.getElementsByClassName("pos-slider-class")[0].addEventListener("click",
 function leftSlide() {
     let counterSlides = parseInt(document.getElementsByClassName("num")[0].textContent);
     document.getElementById("pos-slider").style.transform = "translateX(-10px)";
-    for (let i = 0; i <= 1000; i += 100) {
-        let m = 1;
+    let m = 1;
+    for (let i = 0; i <= 500; i += 50) {
         setTimeout(() => {
-            m-=0.1
-            document.getElementById("slider-img").style.opacity = `${m}`;
-            console.log(m,i)
+            m -= 0.1
+            document.getElementById("slider-img").style.opacity = m;
         }, i)
     }
-    if (counterSlides > 0) {
-        counterSlides--;
-        document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
-    } else {
-        counterSlides = allElements.length - 1;
-        document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
-    }
     setTimeout(() => {
-        for (let i = 0; i < 1000; i += 100) {
-            let m = 0;
+        if (counterSlides > 0) {
+            counterSlides--;
+            document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
+        } else {
+            counterSlides = allElements.length - 1;
+            document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
+        }
+        document.getElementsByClassName("num")[0].textContent = counterSlides;
+        document.getElementById("pos-slider").style.transform = "translateX(0px)";
+        for (let i = 0; i <= 500; i += 50) {
             setTimeout(() => {
-                m+=0.1
-                document.getElementById("slider-img").style.opacity = `${m}`;
+                m += 0.1
+                document.getElementById("slider-img").style.opacity = m;
             }, i)
         }
-        document.getElementById("pos-slider").style.transform = "translateX(0px)";
     }, 1000)
-    document.getElementsByClassName("num")[0].textContent = counterSlides;
 }
 let allElementsRight = []
 
@@ -60,12 +58,31 @@ let helper = []
 helper.unshift(counterSlidesRight)
 
 function rightSlide() {
-    if (helper[0] < allElementsRight.length - 1 && helper[0] !== allElementsRight.length - 1) {
-        helper[0] += 1;
-        document.getElementById("slider-img").src = allElementsRight[helper[0]];
-    } else {
-        helper[0] = 0;
-        document.getElementById("slider-img").src = allElementsRight[0];
+    document.getElementById("pos-slider").style.transform = "translateX(10px)";
+    let m = 1;
+    for (let i = 0; i <= 500; i += 50) {
+        setTimeout(() => {
+            m -= 0.1
+            document.getElementById("slider-img").style.opacity = m;
+        }, i)
     }
+    setTimeout(() => {
+        counterSlidesRight = parseInt(document.getElementsByClassName("num")[0].textContent)
+        helper.unshift(counterSlidesRight)
+        if (helper[0] < allElementsRight.length - 1 && helper[0] !== allElementsRight.length - 1) {
+            helper[0] += 1;
+            document.getElementById("slider-img").src = allElementsRight[helper[0]];
+        } else {
+            helper[0] = 0;
+            document.getElementById("slider-img").src = allElementsRight[0];
+        }
+        document.getElementById("pos-slider").style.transform = "translateX(0px)";
+        for (let i = 0; i <= 500; i += 50) {
+            setTimeout(() => {
+                m += 0.1
+                document.getElementById("slider-img").style.opacity = m;
+            }, i)
+        }
+    }, 1000)
     document.getElementsByClassName("num")[0].textContent = helper[0];
 }
