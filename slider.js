c4bb6656dd2ce6904735585032ab2da1617bad45
Artemissssss <64923345+Artemissssss@users.noreply.github.com>
2021-11-29 17:17:29 +0200

    Add files via upload

--- slider.js
+++ slider.js
@@ -20,7 +20,16 @@ setInterval(() => {
 document.getElementsByClassName("pos-slider-class")[0].addEventListener("click", leftSlide);
 
 function leftSlide() {
-    let counterSlides =  parseInt(document.getElementsByClassName("num")[0].textContent);
+    let counterSlides = parseInt(document.getElementsByClassName("num")[0].textContent);
+    document.getElementById("pos-slider").style.transform = "translateX(-10px)";
+    for (let i = 0; i <= 1000; i += 100) {
+        let m = 1;
+        setTimeout(() => {
+            m-=0.1
+            document.getElementById("slider-img").style.opacity = `${m}`;
+            console.log(m,i)
+        }, i)
+    }
     if (counterSlides > 0) {
         counterSlides--;
         document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
@@ -28,6 +37,16 @@ function leftSlide() {
         counterSlides = allElements.length - 1;
         document.getElementById("slider-img").src = `${allElements[counterSlides]}`;
     }
+    setTimeout(() => {
+        for (let i = 0; i < 1000; i += 100) {
+            let m = 0;
+            setTimeout(() => {
+                m+=0.1
+                document.getElementById("slider-img").style.opacity = `${m}`;
+            }, i)
+        }
+        document.getElementById("pos-slider").style.transform = "translateX(0px)";
+    }, 1000)
     document.getElementsByClassName("num")[0].textContent = counterSlides;
 }
 let allElementsRight = []
@@ -36,9 +55,10 @@ for (let i = 0; i < document.getElementsByClassName("files-slider").length; i++)
     allElementsRight.unshift(document.getElementsByClassName("files-slider")[i].src)
 }
 document.getElementsByClassName("pos-slider-class-right")[0].addEventListener("click", rightSlide);
-let counterSlidesRight =  parseInt(document.getElementsByClassName("num")[0].textContent)
+let counterSlidesRight = parseInt(document.getElementsByClassName("num")[0].textContent)
 let helper = []
 helper.unshift(counterSlidesRight)
+
 function rightSlide() {
     if (helper[0] < allElementsRight.length - 1 && helper[0] !== allElementsRight.length - 1) {
         helper[0] += 1;
